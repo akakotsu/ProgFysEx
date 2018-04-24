8445529ae2cdf0001ce79cb7d571399e258ced3c
akakotsu <38671722+akakotsu@users.noreply.github.com>
2018-04-24 17:03:09 +0200

    Add files via upload

---
+++ Neuron.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+
+class neuron{
+public:
+	neuron(vector<float> tw,float bias);
+	neuron(int);
+	~neuron();
+	void setWeights(vector<float>);
+	void setBias(float);
+
+	vector<float> getWeights();
+	float getBias();
+	int getNumberOfInputs();
+	float sigmoid(float);				
+	float dsigmoid(float);					
+	float activateFunc(vector <float >);			
+	float resultFunc(vector <float >);	
+	void operator()(vector<float> pi) { resultFunc(pi); }
+;	
+protected:
+	vector<float> Weights;
+	float Bias;
+};
