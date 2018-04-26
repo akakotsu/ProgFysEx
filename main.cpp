b76aa198eb4e31bdd8319fee7fc13a45ac2edaf9
akakotsu <38671722+akakotsu@users.noreply.github.com>
2018-04-26 17:19:42 +0200

    test run

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
 #include "Layer.h"
+#include<chrono>
 
+float randomize(float Minimum, float Maximum)
+{
+	random_device RandomDevice; //Initializes random engine
+	mt19937 Generator(RandomDevice()); //Mersenne Twister 19937 generator, rng
+	uniform_real_distribution<> Distribution(Minimum, Maximum); //uniform probability distribution between Minimum and Maximum
+	return Distribution(Generator); //Generate random weights
+}
 
 int main() {
 	float x = 1;
-	float y = 0;;
-	//layer pi(2,2);
-	layer pi({ {&x,&y},{&y,&x} }, { &x,&x });
-	cout << *pi.getWeights()[0][0] << endl;
-	pi.setWeights(pi.getWeights());
-	cout << *pi.getWeights()[0][0] << endl;
-	cout << *pi.resultFunc({ { &x,&y },{&y,&x} })[0] << endl;
+	float y = 0;
+	auto start = std::chrono::system_clock::now();
+	int NS = 150;
+	int WS = 728;
+	layer pi(NS,WS);
+	//cout << *pi.getWeights()[0][0] << endl;
+	//pi.setWeights(pi.getWeights());
+	//cout << *pi.getWeights()[0][0] << endl;
+	//cout << *pi.resultFunc({ { &x,&y },{&y,&x} })[0] << endl;
+	vector<vector<fp>> LW = pi.getWeights();
+	vector<fp> LB = pi.getBias();
+	int loopsize = 100;
+	float random1;
+	float random2;
+	for (int i=0; i < loopsize;i++)
+	{
+		cout << "loop: " << i << endl;
+		for (int j = 0; j < NS; j++)
+		{
+			for (int k = 0; k < WS; k++)
+			{
+				random1 = randomize(-10, 10);
+				LW[j][k] = &random1;
+			}
+			random2 = randomize(-10, 10);
+			LB[j] = &random2;
+		}
+
+	}
+	auto end = std::chrono::system_clock::now();
+	cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << endl;
 	return 0;
 }
